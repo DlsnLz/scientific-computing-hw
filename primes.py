74a6131743359efca54a77958adf598888507b9d
DlsnLz <dilson122009@hotmail.com>
2016-02-18 11:18:11 -0500

    18 Feb

--- primes.py
+++ primes.py
@@ -1,13 +1,26 @@
-def minimo (x,y)
-	if x<y
+# esta funcion define si un numero es menor qu otro.
+
+def minimo (x,y):
+	if x < y:
 		return x
-	else 
+	else:
 		return y
-s = minime (2,7)
 
-
-def mcd (x,y)
+def mcd (x,y):
 	m = minimo(x,y)
-	for i in (m,0,-1):
-		if x%i == 0 and y%i == 0
-		return i
+	for i in range (m,0,-1):
+		if x % i == 0 and y % i == 0:
+			return i
+# esta funcion define si un numero es comprimo o no.
+
+def arecoprime (a,b):
+
+	m = mcd (a,b)
+
+	if m==1:
+		return 1
+	else:
+		return 0
+
+
+print arecoprime (10,12)
