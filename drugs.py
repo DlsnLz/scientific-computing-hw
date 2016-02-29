8c1e76ae7b0121b24f2f9d84243cfacd0bc8219c
DlsnLz <dilson122009@hotmail.com>
2016-02-29 00:31:01 -0500

    Taller 1

---
+++ drugs.py
@@ -0,0 +1,37 @@
+# Funcion que identificara el tiempo en el que el cuerpo liberara la droga consumida por el paciente.
+# Feb - 25 -2016
+# Dlsn Lzro
+
+# Librerias Necesarias importadas
+
+import numpy
+import matplotlib
+import matplotlib.pyplot
+
+# Datos de entrada.
+
+k = 0.2
+d = 0.1
+q0 = 200
+aux = q0
+
+# Datos que me fabricaran la grafica.
+
+y = range (101)
+t = range (101)
+
+# Funcion que me imprimira los resultados numericos por pantalla.
+
+for i in range(101):
+	q = q0-k*d*q0
+	q0 = q
+	y[i] = q
+
+ynew=numpy.array(y)
+tnew=numpy.array(t)
+
+print ynew
+print tnew
+
+matplotlib.pyplot.plot(tnew,ynew)
+matplotlib.pyplot.show()
