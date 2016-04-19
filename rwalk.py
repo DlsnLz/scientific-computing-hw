8a765c094f5d272d39f57b7076d3c93d15f67a9d
DlsnLz <dilson122009@hotmail.com>
2016-04-19 11:23:28 -0500

    martes

--- rwalk.py
+++ rwalk.py
@@ -1,4 +1,20 @@
 import numpy as np
+import matplotlib as mplt
+import matplotlib.pyplot as plt
 
 np.random.seed(0)
-print np.random.random(12)
+
+#Constantes
+L = 3.
+R = 1.
+N = 10000
+
+# generando los puntos
+n = 0
+for i in range(N):
+	
+	x = L * np.random.random() -0.5 * L
+	y = L * np.random.random() -0.5 * L
+	
+	if x**2
+
